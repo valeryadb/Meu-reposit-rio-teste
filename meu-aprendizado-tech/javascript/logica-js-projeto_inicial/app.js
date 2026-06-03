4c067b15b38b8e440e262c6f428e175db1da4da1
valeryadb <valeryadev@gmail.com>
2026-06-02 23:31:25 -0300

    JavaScript, ajustando o javascript

---
+++ meu-aprendizado-tech/javascript/logica-js-projeto_inicial/app.js
@@ -0,0 +1,8 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número entre 1 a 10')
+
+if(chute == numeroSecreto) {
+    console.log('Isso ai! Você desconriu o número secreto (5)')
+}
+
