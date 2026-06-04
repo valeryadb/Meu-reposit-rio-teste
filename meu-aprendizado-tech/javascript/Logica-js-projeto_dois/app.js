e9edb1a82d4bc726e648e538b2a30d32ddaccb65
valeryadb <valeryadias15@gmail.com>
2026-06-03 22:28:31 -0300

    Ajustando o javascript

---
+++ meu-aprendizado-tech/javascript/Logica-js-projeto_dois/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número de 1 e 10';
