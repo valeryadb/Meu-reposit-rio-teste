6d506f1c58a238dc4c1b2877547315e8785b6227
Valérya Dias <129337623+valeryadb@users.noreply.github.com>
2026-06-10 19:37:43 -0300

    Update app.js

--- meu-aprendizado-tech/javascript/Logica-js-projeto_dois/app.js
+++ meu-aprendizado-tech/javascript/Logica-js-projeto_dois/app.js
@@ -52,18 +52,7 @@ function gerarNumeroAleatorio() {
     }
 }
 
-function limparCampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
-}
 
-function reiniciarJogo() {
-    numeroSecreto = gerarNumeroAleatorio();
-    limparCampo();
-    tentativas = 1;
-    exibirMensagemInicial();
-    document.getElementById('reiniciar').setAttribute('disabled', true)
-}
 
 
 
