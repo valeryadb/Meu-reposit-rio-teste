453809660eb1fd437f79c05ec3303f3a7caf4d16
Valérya Dias <129337623+valeryadb@users.noreply.github.com>
2026-06-23 22:49:35 -0300

    Update app.js

--- meu-aprendizado-tech/javascript/Logica-js-projeto_dois/app.js
+++ meu-aprendizado-tech/javascript/Logica-js-projeto_dois/app.js
@@ -13,8 +13,26 @@ function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
+exibirMensagemInicial();
 
-
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 
 
 
